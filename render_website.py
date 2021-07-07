d25e0e8443741b06a9ea6f7c09f7469856a7c51c
Mais1212 <64730510+Mais1212@users.noreply.github.com>
2021-07-07 23:47:13 +0300

    Dried the code

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pathlib
 
 from jinja2 import Environment, FileSystemLoader
 from livereload import Server
@@ -7,7 +8,7 @@ from more_itertools import chunked
 
 
 def get_book_json(json_path):
-    json_path = f"library\\json\\{json_path}"
+    json_path = pathlib.Path("library", "json", json_path)
 
     with open(json_path, 'r', encoding="utf-8") as book_json:
         book_json = json.load(book_json)
@@ -15,9 +16,11 @@ def get_book_json(json_path):
     img_path, book_title, book_path = book_json["img_path"],\
         book_json["title"], book_json['book_path']
 
-    book_json["title"] = make_correct_book_title(book_title)
-    book_json["img_path"] = make_correct_img_path(img_path)
-    book_json["book_path"] = make_correct_book_path(book_path)
+    img_path = f"../{img_path}" if img_path else "../images/Noimg.gif"
+
+    book_json["title"] = book_title
+    book_json["img_path"] = img_path
+    book_json["book_path"] = f"../{book_path}"
 
     return book_json
 
@@ -27,31 +30,6 @@ def split_into_columns(book_coulmn):
     first_book_column, second_book_coulmn = list(chunked(book_coulmn, num))
 
     return first_book_column, second_book_coulmn
-
-
-def make_correct_book_path(book_path):
-    book_path = book_path.replace("\\", "/")
-    book_path = f"../{book_path}"
-
-    return book_path
-
-
-def make_correct_img_path(img_path):
-    if img_path is None:
-        img_path = "images/Noimg.gif"
-
-    img_path = img_path.replace("\\", "/")
-    img_path = f"../{img_path}"
-
-    return img_path
-
-
-def make_correct_book_title(book_title):
-    splited_book_title = book_title.split()
-    splited_book_title.pop(-1)
-    book_title = ' '.join(splited_book_title)
-
-    return book_title
 
 
 def get_books_json():
@@ -68,9 +46,10 @@ def rebuild():
     template = environment.get_template("template.html")
     books_json = get_books_json()
     chunked_books_per_page = list(chunked(books_json, books_number_per_page))
+    path = pathlib.Path("pages", "index")
 
     for current_page, book_coulmn in enumerate(chunked_books_per_page, 1):
-        file_name = f"pages\\index{current_page}.html"
+        file_name = f"{path}{current_page}.html"
         first_book_column, second_book_coulmn = split_into_columns(book_coulmn)
 
         pages_info = {
